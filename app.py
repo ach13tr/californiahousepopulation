19a1b58d424c362eee157d49e7527f1ecbdd5c1d
Achal Trivedi <achaltrivedi1325@gmail.com>
2024-06-21 16:05:11 +0530

    Assignment

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.fit_transform(print(np.array(list(data.values())).reshape(1,-1)))
+    new_data=scalar.transform((np.array(list(data.values())).reshape(1,-1)))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -27,12 +27,10 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transfrom(np.array(data).reshape(1,-1))
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is{}".format(output))
-
-
 
 if __name__=="__main__":
     app.run(debug=True)
